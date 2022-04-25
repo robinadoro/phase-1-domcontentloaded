8c6dfc2d87f1cb4c4da1a61bc4c545de66893fb3
Robinson Idagiza <robinadoro@gmail.com>
2022-04-25 10:32:57 +0300

    commit tree 603f5fb22ba14ca2688a7dd4600a201941f90a86

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+// document.querySelector('p').innerText=("JavaScript is so cool. It lets me add text to my page programmatically.")
+
+document.addEventListener('DOMContentLoaded', function(){
+    events()
+})
+function events(){
+    document.getElementById('text').innerHTML='This is really cool!'
+}
+
